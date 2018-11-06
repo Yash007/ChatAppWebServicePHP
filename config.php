d4b4c8ffb0405eb2700d4a23c9d07328fe6949d9
Yash Sompura <sompurayash@live.com>
2018-11-06 14:44:45 -0500

    Database Connection File

--- config.php
+++ config.php
@@ -6,7 +6,8 @@
         Purpose: Cybersecurity Fall 2018 Computer Network
         Timeline: Sept 04, 2018 to Dec 14, 2018
     */
-    defing("WEB_URL","");
+
+    define("WEB_URL","");
     define("HOST_NAME","localhost");
     define("DB_NAME","chatapp");
     define("DB_USER","root");
