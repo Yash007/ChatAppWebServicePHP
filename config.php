9061ec9f259591ada3d3aca5f6f1e2673cf99356
Yash Sompura <sompurayash@live.com>
2018-11-06 14:42:27 -0500

    config.php file with Constants

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+    /*
+        Project Name: Chat app with Two factor Authentication
+        Developed By: Yashkumar Sompura
+                      Varshali Nagrale
+        Purpose: Cybersecurity Fall 2018 Computer Network
+        Timeline: Sept 04, 2018 to Dec 14, 2018
+    */
+    defing("WEB_URL","");
+    define("HOST_NAME","localhost");
+    define("DB_NAME","chatapp");
+    define("DB_USER","root");
+    define("DB_PASSWORD","root");
+    define("DB_PORT","3306");
+?>
