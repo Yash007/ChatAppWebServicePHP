753ba0c7cb47c5b3b3228a792fbcf500f1fa3a70
Yash Sompura <sompurayash@live.com>
2018-11-07 14:41:04 -0500

    [Task] - Signup implementation

--- users.php
+++ users.php
@@ -24,7 +24,79 @@
         }
 
         function signUpUser()   {
+            $data = json_decode(file_get_contents("php://input"),true);
+            $uFirstName = $data['uFirstName'];
+            $uLastName = $data['uLastName'];
+            $uEmail = $data['uEmail'];
+            $uPassword = $data['uPassword'];
+            $uMobile = $data['uMobile'];
+            $uPassword = sha1($uPassword);
+
+            $sql = "select count(uId) as total from users where uEmail='$uEmail'";
+            $res = mysqli_query($this->link,$sql) or die("Error in Count query");
+            $row = mysqli_fetch_assoc($res);
+            $total = $row['total'];
             
+            $validate = true;
+            if(empty($uFirstName) || $uFirstName == " ")    {
+                $validate = false;
+                $result = array();
+                $result['result'] = "Error";
+                $result['message'] = "Firstname required!!";
+            }
+
+            if(empty($uLastName) || $uLastName == " ")    {
+                $validate = false;
+                $result = array();
+                $result['result'] = "Error";
+                $result['message'] = "Lastname required!!";
+            }
+
+            if(empty($uEmail) || $uEmail == " ")    {
+                $validate = false;
+                $result = array();
+                $result['result'] = "Error";
+                $result['message'] = "Email required!!";
+            }
+
+            if(empty($uMobile) || $uMobile == " ")    {
+                $validate = false;
+                $result = array();
+                $result['result'] = "Error";
+                $result['message'] = "Mobile number required!!";
+            }
+
+            if(empty($uPassword) || $uPassword == " ")    {
+                $validate = false;
+                $result = array();
+                $result['result'] = "Error";
+                $result['message'] = "Password required!!";
+            }
+
+            if($validate == true && $total == 0) {
+                $sql = "insert into users(uFirstName,uLastName,uEmail,uMobile,uHash) values('$uFirstName','$uLastName','$uEmail','$uMobile','$uPassword')";
+                $res = mysqli_query($this->link,$sql) or die("Error in Signup User query");
+                $res = mysqli_affected_rows($this->link);
+    
+                if($res)    {
+                    $result = array();
+                    $result['result'] = "Success";
+                    $result['message'] = "User has been signed up.";
+                }
+                else    {
+                    $result = array();
+                    $result['result'] = "Error";
+                    $result['message'] = "User has not been signed up.";
+                }
+            }
+            else    {
+                $result = array();
+                $result['result'] = "Error";
+                $result['message'] = "User has already registered!!";
+            }
+
+            header("Content-type: Application/json");
+            echo json_encode($result);
         }
 
         function loginUser()    {
