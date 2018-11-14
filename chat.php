00fb8a113ebd35e2fc8876ca6478fb629d8a95df
Yash Sompura <sompurayash@live.com>
2018-11-14 18:19:02 -0500

    [Task] Send Message Implementation but AI stuff remaining

--- chat.php
+++ chat.php
@@ -2,6 +2,10 @@
     include("codebase/Connection.php");
     $chat = new Chat();
     
+    if(isset($_GET['chatList']) && $_GET['chatList'] == "true") {
+        $chat -> chatList();
+    }
+
     if(isset($_GET['sendChat']) && $_GET['sendChat'] == "true") {
         $chat -> sendChat();
     }
@@ -34,10 +38,41 @@
 		function __construct()	{
 			$this -> conObj = new Connection();
             $this -> link = $this -> conObj -> dbConnect();
+            date_default_timezone_set('US/Eastern');
+        }
+
+        function chatList() {
+            
         }
 
         function sendChat() {
+            $data = json_decode(file_get_contents("php://input"),true);
+            $senderId = $data['senderId'];
+            $receiverId = $data['receiverId'];
+            $date = date("Y-m-d");
+            $time = date("H:i:s");
+            $message = mysqli_real_escape_string($this->link,$data['message']);
+            //$senLevel =  (AI code to find Sensitivity Level)
+            $sensLevel = 0;
 
+            if($message != NULL || $message != " " || !empty($message)) {
+                $sql = "insert into messages (mSenderId, mReceiverId, mDate, mTime, mMessage, mSensitiveLevel) values ('$senderId','$receiverId','$date','$time','$message','$sensLevel')";
+                $res = mysqli_query($this->link, $sql) or die("Error in sending Message Query".$sql);
+                $res = mysqli_affected_rows($this->link);
+                if($res)    {
+                    $result = array();
+                    $result['result'] = "Success";
+                    $result['message'] = "Message send successfully";
+                }
+                else    {
+                    $result = array();
+                    $result['result'] = "Error";
+                    $result['message'] = "Failed to send message! Please try again.";
+                }
+            }
+
+            header("Content-type: Application/json");
+            echo json_encode($result);
         }
 
         function receiveChat()  {
@@ -57,8 +92,7 @@
         }
 
         function loadChat() {
-
+            
         }
-    
     }
 ?>
