ecb1c5ddb2f180cdc70baa6808526554c92b2e06
Yash Sompura <sompurayash@live.com>
2018-11-14 17:14:37 -0500

    [Task] Add Contact Implementation

--- contacts.php
+++ contacts.php
@@ -28,7 +28,39 @@
         }
 
         function addContact()  {
+            $data = json_decode(file_get_contents("php://input"),true);
+            $sourceId = $data['sourceId'];
+            $destinationId = $data['destinationId'];
 
+            $sql = "select count(cId) as total from contacts where cSourceId = '$sourceId' and cDestinationId = '$destinationId'";
+            $res = mysqli_query($this->link, $sql) or die("Error in Authenticating Contact Count!!".$sql);
+            $row = mysqli_fetch_assoc($res);
+            $total = $row['total'];
+            if($total ==  0)   {
+                //Add contact to your list
+                $sql = "insert into contacts (cSourceId, cDestinationId) values ('$sourceId','$destinationId')";
+                $res = mysqli_query($this->link,$sql) or die("Error in adding Contact query.".$sql);
+                $res = mysqli_affected_rows($this->link);
+                if($res)    {
+                    $result = array();
+                    $result['result'] = "Success";
+                    $result['message'] = "Contact has been saved successfully.";
+                }
+                else    {
+                    $result = array();
+                    $result['result'] = "Error";
+                    $result['message'] = "Error in adding user in contacts. Please try again!";
+                }
+            }
+            else    {
+                //Contact already Exists!!
+                $result = array();
+                $result['result'] = "Error";
+                $result['message'] = "User already in your contact list!";
+            }
+
+            header("Content-type: Application/json");
+            echo json_encode($result);
         }
 
         function removeContact()    {
